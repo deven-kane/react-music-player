f8c49e99a0568a4faaf7ddc0ed05061cc18c7a37
deven-kane <72709512+deven-kane@users.noreply.github.com>
2022-04-11 16:05:02 -0700

    add useEffect to update active song selection on library UI when skipping

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ function App() {
         songInfo={songInfo}
         songs={songs}
         setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
       />
       <Library
         audioRef={audioRef}
